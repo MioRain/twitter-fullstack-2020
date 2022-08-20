3faa68685fc4b16c2572d356aa37d448f7bdbc45
AndyChienn <andy2002chien@hotmail.com.tw>
2022-08-20 16:45:08 +0800

    feat:add publicChatRoom view

---
+++ controllers/chat-controlller.js
@@ -0,0 +1,14 @@
+// 登入、註冊、登出、拿到編輯頁、送出編輯
+const assert = require('assert')
+const bcrypt = require('bcryptjs')
+const { User, Tweet, Reply, Like, Followship } = require('../models')
+const helpers = require('../_helpers')
+
+const chatController = {
+  renderPublicChatRoom: (req, res) => {
+    return res.render('publicChatRoom')
+  }
+
+}
+
+module.exports = chatController
